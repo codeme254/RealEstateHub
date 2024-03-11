cc1b5e6b04ad4c2c2f47b7d8e33668ea21c96084
codeme254 <otwomadennis365@gmail.com>
2024-03-11 16:33:09 +0300

    connect the api with the database

--- api/index.js
+++ api/index.js
@@ -1,23 +1,24 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
-dotenv.config()
+dotenv.config();
 const app = express();
 
 app.get("/", (_, res) => {
-    res.json({message: "Welcome to RealEstate API..."})
-})
+  res.json({ message: "Welcome to RealEstate API..." });
+});
 
-mongoose.connect(process.env.MONGO_URI).then(
-    () => {
-        console.log("Successfully connected to the database...")
-    }
-).catch((error) => {
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Successfully connected to the database...");
+  })
+  .catch((error) => {
     console.log("There was an error connecting to the database");
     console.error(error);
-})
+  });
 
 app.listen(3000, () => {
-    console.log(`Server running on port 3000...`);
-})
+  console.log(`Server running on port 3000...`);
+});
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,40 +1,43 @@
 import mongoose from "mongoose";
 
-const userSchema = mongoose.Schema({
+const userSchema = mongoose.Schema(
+  {
     firstName: {
-        type: String,
-        required: true,
-        max: 40,
-        min: 2
+      type: String,
+      required: true,
+      max: 40,
+      min: 2,
     },
     lastName: {
-        type: String,
-        required: true,
-        max: 40,
-        min: 2
+      type: String,
+      required: true,
+      max: 40,
+      min: 2,
     },
     username: {
-        type: String,
-        required: true,
-        min: 2,
-        max: 20
+      type: String,
+      required: true,
+      min: 2,
+      max: 20,
     },
     emailAddress: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     userType: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     profilePictureUrl: {
-        type: String,
+      type: String,
     },
     password: {
-        type: String,
-        required: true
-    }
-}, { timestamps: true});
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
 
 const User = mongoose.model("User", userSchema);
 export default User;

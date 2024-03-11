531dd4a2acbb4f5bce6d4eb7f7283d13f444e3df
codeme254 <otwomadennis365@gmail.com>
2024-03-11 16:28:26 +0300

    connect the api with the database

--- api/index.js
+++ api/index.js
@@ -1,9 +1,21 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
+dotenv.config()
 const app = express();
 
 app.get("/", (_, res) => {
     res.json({message: "Welcome to RealEstate API..."})
+})
+
+mongoose.connect(process.env.MONGO_URI).then(
+    () => {
+        console.log("Successfully connected to the database...")
+    }
+).catch((error) => {
+    console.log("There was an error connecting to the database");
+    console.error(error);
 })
 
 app.listen(3000, () => {

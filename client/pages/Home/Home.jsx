a21483814d80a69f22076912f125cfeb28e378f8
codeme254 <otwomadennis365@gmail.com>
2024-03-13 15:48:46 +0300

    create the hero section for desktop view

--- client/pages/Home/Home.jsx
+++ client/pages/Home/Home.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import "./home.css";
+import { Link } from "react-router-dom";
 
 function Home() {
-  return <div>This is the home page</div>;
+  return (
+    <div className="home">
+      <div className="home__text-box">
+        <h2 className="home__title">
+          <span className="home__title--sub">Find your next</span>
+          <span className="home__title--main">Dream Home</span>
+        </h2>
+        <Link to="/sign-in" className="hero__cta">
+          Get Started
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Home;

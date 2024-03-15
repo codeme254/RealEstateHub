290244fffbdad38c34dda774e844e7ff43c24b5d
codeme254 <otwomadennis365@gmail.com>
2024-03-15 15:03:27 +0300

    automatically redirect logged in users to profile
    Previously, when the site loaded, the default page was the home page,
    with this implementation, logged in users are redirected to their
    profile as opposed to previously where logged in users still landed on
    the default homepage, in the future, logged in users will be redirected
    to a listings page

--- client/pages/Home/Home.jsx
+++ client/pages/Home/Home.jsx
@@ -1,8 +1,14 @@
-import React, { useState, useRef } from "react";
+import React, { useEffect } from "react";
 import "./home.css";
-import { Link } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Home() {
+  const user = useSelector((state) => state.user.value);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (user) navigate("/profile");
+  }, [user]);
   return (
     <div className="home">
       <div className="home__text-box">

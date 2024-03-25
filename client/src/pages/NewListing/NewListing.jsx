6d846297592f11d109ea1443fcb883d0a925bb01
codeme254 <otwomadennis365@gmail.com>
2024-03-25 11:32:23 +0300

    manage form data state except the images

--- client/src/pages/NewListing/NewListing.jsx
+++ client/src/pages/NewListing/NewListing.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "./new-listing.css";
 
 function NewListing() {
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+    address: "",
+    type: "rent",
+    offer: false,
+    regularPrice: 0,
+    discountedPrice: 0,
+    bedrooms: 0,
+    bathrooms: 0,
+    wifi: false,
+    parking: false,
+    swimmingPool: false,
+    furnished: false,
+    gym: false,
+  });
+  const handleChange = (e) => {
+    if (e.target.id === "sale" || e.target.id == "rent") {
+      setFormData({
+        ...formData,
+        type: e.target.id,
+      });
+    }
+    if (
+      e.target.id === "wifi" ||
+      e.target.id === "parking" ||
+      e.target.id === "swimmingPool" ||
+      e.target.id === "furnished" ||
+      e.target.id === "gym" ||
+      e.target.id === "offer"
+    ) {
+      setFormData({ ...formData, [e.target.id]: e.target.checked });
+    }
+
+    if (
+      e.target.type === "number" ||
+      e.target.type === "text" ||
+      e.target.type === "textarea"
+    ) {
+      setFormData({ ...formData, [e.target.id]: e.target.value });
+    }
+  };
   return (
     <div className="new-listing">
       <h2 className="create-listing-title">Create a listing</h2>
@@ -15,6 +57,8 @@ function NewListing() {
             id="title"
             className="new-listing__form-group--text-input"
             placeholder="title"
+            onChange={handleChange}
+            value={formData.title}
           />
         </div>
         <div className="new-listing__form-group">
@@ -26,6 +70,8 @@ function NewListing() {
             id="address"
             className="new-listing__form-group--text-input"
             placeholder="address"
+            onChange={handleChange}
+            value={formData.address}
           />
         </div>
 
@@ -40,6 +86,8 @@ function NewListing() {
             id="description"
             className="new-listing-text-area"
             placeholder="description"
+            onChange={handleChange}
+            value={formData.description}
           ></textarea>
         </div>
 
@@ -53,6 +101,8 @@ function NewListing() {
               name="type"
               id="rent"
               className="new-listing__form-group--radio"
+              onChange={handleChange}
+              checked={formData.type === "rent"}
             />
           </div>
           <div className="radio-group">
@@ -64,8 +114,23 @@ function NewListing() {
               name="type"
               id="sale"
               className="new-listing__form-group--radio"
+              onChange={handleChange}
+              checked={formData.type === "sale"}
             />
           </div>
+        </div>
+
+        <div className="radio-group mb-fix">
+          <label htmlFor="offer" className="new-listing__form-group--label">
+            offer
+          </label>
+          <input
+            type="checkbox"
+            id="offer"
+            className="new-listing__form-group--radio"
+            onChange={handleChange}
+            checked={formData.offer}
+          />
         </div>
 
         <div className="new-listing__form-group--flex">
@@ -81,22 +146,28 @@ function NewListing() {
               id="regularPrice"
               className="new-listing__form-group--text-input"
               placeholder="regular price"
+              onChange={handleChange}
+              value={formData.regularPrice}
             />
           </div>
-          <div className="new-listing__form-group">
-            <label
-              htmlFor="discountedPrice"
-              className="new-listing__form-group--label"
-            >
-              discounted price (kshs)
-            </label>
-            <input
-              type="number"
-              id="discountedPrice"
-              className="new-listing__form-group--text-input"
-              placeholder="discounted price"
-            />
-          </div>
+          {formData.offer === true && (
+            <div className="new-listing__form-group">
+              <label
+                htmlFor="discountedPrice"
+                className="new-listing__form-group--label"
+              >
+                discounted price (kshs)
+              </label>
+              <input
+                type="number"
+                id="discountedPrice"
+                className="new-listing__form-group--text-input"
+                placeholder="discounted price"
+                onChange={handleChange}
+                value={formData.discountedPrice}
+              />
+            </div>
+          )}
         </div>
 
         <div className="new-listing__form-group--flex">
@@ -106,9 +177,11 @@ function NewListing() {
             </label>
             <input
               type="number"
-              id="beds"
+              id="bedrooms"
               className="new-listing__form-group--text-input"
               placeholder="number of bedrooms"
+              onChange={handleChange}
+              value={formData.bedrooms}
             />
           </div>
 
@@ -118,9 +191,11 @@ function NewListing() {
             </label>
             <input
               type="number"
-              id="baths"
+              id="bathrooms"
               className="new-listing__form-group--text-input"
               placeholder="number of bathrooms"
+              onChange={handleChange}
+              value={formData.bathrooms}
             />
           </div>
         </div>
@@ -128,23 +203,48 @@ function NewListing() {
         <div className="new-listing__form-group--flex mb-fix">
           <div className="checkbox-group">
             <label htmlFor="wifi">wifi</label>
-            <input type="checkbox" id="wifi" />
+            <input
+              type="checkbox"
+              id="wifi"
+              onChange={handleChange}
+              checked={formData.wifi === true}
+            />
           </div>
           <div className="checkbox-group">
             <label htmlFor="parking">parking</label>
-            <input type="checkbox" id="parking" />
+            <input
+              type="checkbox"
+              id="parking"
+              onChange={handleChange}
+              checked={formData.parking === true}
+            />
           </div>
           <div className="checkbox-group">
             <label htmlFor="swimmingPool">swimming pool</label>
-            <input type="checkbox" id="swimmingPool" />
+            <input
+              type="checkbox"
+              id="swimmingPool"
+              onChange={handleChange}
+              checked={formData.swimmingPool === true}
+            />
           </div>
           <div className="checkbox-group">
             <label htmlFor="furnished">furnished</label>
-            <input type="checkbox" id="furnished" />
+            <input
+              type="checkbox"
+              id="furnished"
+              onChange={handleChange}
+              checked={formData.furnished === true}
+            />
           </div>
           <div className="checkbox-group">
             <label htmlFor="gym">gym</label>
-            <input type="checkbox" id="gym" />
+            <input
+              type="checkbox"
+              id="gym"
+              onChange={handleChange}
+              checked={formData.gym === true}
+            />
           </div>
         </div>
         <div className="new-listing__form-group">
@@ -170,4 +270,5 @@ function NewListing() {
   );
 }
 
+// https://www.youtube.com/watch?v=ZdAcKVWA520
 export default NewListing;
